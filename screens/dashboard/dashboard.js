543f5529d3951a1abaab9fed8d637480366a71a2
Oluwasuyi Babayomi <yomzeew@gmail.com>
2024-09-17 13:33:51 +0100

    Merge branch 'main' of https://github.com/Stulivery/mobile-app

--- screens/dashboard/dashboard.js
+++ screens/dashboard/dashboard.js
@@ -1,10 +1,19 @@
+import { Image, Text, View } from "react-native"
 import { height, width } from "../../constants/mobileDimensions"
 
 const Dashbaord=()=>{
     return(
         <>
+        
         <View style={{height:height,width:width}} className="px-5 py-[88px]">
-            
+            <View>
+            <Image
+                            className="h-12 w-12"
+                            source={require("../../assets/images/logo.png")}
+                            resizeMode="contain"
+                        />
+            </View>
+
 
         </View>
         </>
