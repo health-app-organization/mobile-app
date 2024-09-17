d4698fe59c14a255f3ce9495814335bae1431b73
Ayomide Akintan <emzyakints2005@gmail.com>
2024-09-17 13:36:08 +0100

    update

--- screens/onboarding/loginandforgetpass.js
+++ screens/onboarding/loginandforgetpass.js
@@ -51,7 +51,7 @@ export const Login = () => {
         // IF first time login
         // THEN redirect to id verification
         // ELSE navigate to dashboard
-        navigation.navigate("dashboard")
+        navigation.navigate("dashboardscreen")
     }
     const handleToSignup = () => {
         navigation.navigate("signup");
