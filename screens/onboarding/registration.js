acf9b7cdac28555083f3ace3021d17027f07b9b0
Ayomide Akintan <emzyakints2005@gmail.com>
2024-10-24 21:58:10 +0100

    removed unnecsary folder

--- screens/onboarding/registration.js
+++ screens/onboarding/registration.js
@@ -27,11 +27,7 @@ import {
   CustomInputWithHeader,
 } from "../mycomponents/mycomponent";
 import { Box, CustomTextnumber } from "../mycomponents/mycomponent";
-import {
-  primarycolor,
-  whitecolor,
-  greycolortwo,
-} from "../../constants/color";
+import { primarycolor, whitecolor, greycolortwo } from "../../constants/color";
 import { CustomButton } from "../mycomponents/mycomponent";
 import NumericKeyboard from "../modals/CustomKeyboard"; // Importing the Numeric Keyboard
 
@@ -109,218 +105,211 @@ export default function Registration() {
   };
 
   return (
-   
-        <View className="w-full h-full px-5 py-[88px] bg-white">
-            {/* Back button */}
-            {currentStep > 0 && (
-              <TouchableOpacity
-                onPress={handleContinueBackwards}
-                className="absolute -top-9 left-3"
+    <View className="w-full h-full px-5 py-[88px] bg-white">
+      {/* Back button */}
+      {currentStep > 0 && (
+        <TouchableOpacity
+          onPress={handleContinueBackwards}
+          className="absolute -top-9 left-3"
+        >
+          <Feather name="arrow-left" size={24} color="black" />
+        </TouchableOpacity>
+      )}
+
+      {/* Conditional Headers */}
+      <View className="mt-6 flex justify-center items-center">
+        {currentStep === 0 && (
+          <>
+            <Text style={[Textstyles.text_medium]} className="mb-6">
+              Create an account
+            </Text>
+            <Text style={[Textstyles.text_small]} className="text-center mb-6">
+              Please enter your phone number to receive a 6-digit code via SMS
+            </Text>
+          </>
+        )}
+
+        {currentStep === 1 && (
+          <>
+            <Text style={[Textstyles.text_medium]} className="mb-6">
+              Verification
+            </Text>
+            <Text style={[Textstyles.text_small]} className="text-center">
+              Enter the 6-digit code sent to your phone
+            </Text>
+            <View style={{ alignItems: "center", marginTop: 40 }}>
+              <Text style={[Textstyles.text_xsmall]} className="text-red-300">
+                {errorMsg}
+              </Text>
+              <Pressable
+                onPress={handleShowKeys}
+                className="flex-row justify-center items-center"
               >
-                <Feather name="arrow-left" size={24} color="black" />
-              </TouchableOpacity>
-            )}
-
-            {/* Conditional Headers */}
-            <View className="mt-6 flex justify-center items-center">
-              {currentStep === 0 && (
-                <>
-                  <Text style={[Textstyles.text_medium]} className="mb-6">
-                    Create an account
-                  </Text>
-                  <Text
-                    style={[Textstyles.text_small]}
-                    className="text-center mb-6"
+                {otp.map((digit, index) => (
+                  <View
+                    key={index}
+                    style={{ flexDirection: "row", alignItems: "center" }}
                   >
-                    Please enter your phone number to receive a 6-digit code via SMS
-                  </Text>
-                </>
-              )}
-
-              {currentStep === 1 && (
-                <>
-                  <Text style={[Textstyles.text_medium]} className="mb-6">
-                    Verification
-                  </Text>
-                  <Text style={[Textstyles.text_small]} className="text-center">
-                    Enter the 6-digit code sent to your phone
-                  </Text>
-                  <View style={{ alignItems: "center", marginTop: 40 }}>
-                    <Text style={[Textstyles.text_xsmall]} className="text-red-300">
-                      {errorMsg}
-                    </Text>
-                    <Pressable
-                      onPress={handleShowKeys}
-                      className="flex-row justify-center items-center"
-                    >
-                      {otp.map((digit, index) => (
-                        <View
-                          key={index}
-                          style={{ flexDirection: "row", alignItems: "center" }}
-                        >
-                          <Box inputText={digit} />
-                          {index < otp.length - 1 && <View className="w-4" />}
-                        </View>
-                      ))}
-                    </Pressable>
+                    <Box inputText={digit} />
+                    {index < otp.length - 1 && <View className="w-4" />}
                   </View>
-                  <View className="flex-row h-10">
-                    <Text
-                      style={[Textstyles.text_small]}
-                      className="text-center mt-6"
-                    >
-                      Didn't get a code?
-                    </Text>
-                    <TouchableOpacity className="mt-6 ml-1">
-                      <Text
-                        style={[Textstyles.text_small]}
-                        className="text-[#0099b8]"
-                      >
-                        Resend
-                      </Text>
-                    </TouchableOpacity>
-                  </View>
-                </>
-              )}
+                ))}
+              </Pressable>
             </View>
+            <View className="flex-row h-10">
+              <Text
+                style={[Textstyles.text_small]}
+                className="text-center mt-6"
+              >
+                Didn't get a code?
+              </Text>
+              <TouchableOpacity className="mt-6 ml-1">
+                <Text
+                  style={[Textstyles.text_small]}
+                  className="text-[#0099b8]"
+                >
+                  Resend
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
+      </View>
 
-            <View className="h-4" />
+      <View className="h-4" />
 
-            {currentStep === 0 && (
-              <>
-                <View className="mb-5">
-                  <CustomTextnumber
-                    placeholder={"mobile number"}
-                    placeholderTextColor={greycolortwo}
-                  />
-                </View>
-              </>
-            )}
-
-
-            <>
-              {currentStep === 2 && (
-                <> 
-                <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={{ flex: 1 }}
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 10 : 0}  // Increase offset here
-    >
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-                  <View>
-                    <Text
-                      style={[Textstyles.text_medium]}
-                      className="mb-4 text-center"
-                    >
-                      Welcome to HealthApp
-                    </Text>
-                    <View className="w-full h-28 mb-5 flex justify-center items-center">
-                      <View className="w-[117.91px] justify-center flex items-center h-[109.36px] rounded-xl">
-                        <Image
-                          source={require("../../assets/images/pro.png")}
-                          resizeMode="contain"
-                          className="w-full"
-                        />
-                      </View>
-                    </View>
-                  </View>
-
-                  <CustomInputWithHeader
-                    headerText="First name"
-                    placeholder="Enter your first name"
-                    leftIconName="user"
-                    onChange={(text) => console.log(text)}
-                  />
-                  <View className="h-3" />
-                  <CustomInputWithHeader
-                    headerText="Last name"
-                    placeholder="Enter your last name"
-                    leftIconName="user"
-                    onChange={(text) => console.log(text)}
-                  />
-                  <View className="h-3" />
-                  <CustomInputWithHeader
-                    headerText="Email address"
-                    placeholder="Enter your email address"
-                    leftIconName="envelope"
-                    onChange={(text) => console.log(text)}
-                  />
-                  <View className="h-3" />
-                  <CustomInputWithHeader
-                    headerText="Phone number"
-                    placeholder="Enter your phone number"
-                    leftIconName="phone"
-                    onChange={(text) => console.log(text)}
-                  />
-                  <View className="h-3" />
-                  <CustomInputWithHeader
-                    headerText="Date of birth"
-                    placeholder="Enter your date of birth"
-                    leftIconName="calendar"
-                    onChange={(text) => console.log(text)}
-                  />
-                  <View className="h-3" />
-                  <CustomInputWithHeader
-                    headerText="Gender"
-                    placeholder="Enter your Gender"
-                    leftIconName="user"
-                    onChange={(text) => console.log(text)}
-                  />
-                  <View className="h-3" />
-                  <CustomInputpassword
-                    headerText="Password"
-                    placeholder="Enter your password"
-                    value={password}
-                    secureTextEntry={true}
-                    onChange={(text) => setPassword(text)}
-                    leftIconName="lock"
-                    leftIconColor="#000"
-                    leftIconSize={20}
-                  />
-                  <View className="h-3" />
-                  <CustomInputpassword
-                    headerText="Confirm password"
-                    placeholder="Enter your password again"
-                    value={password}
-                    secureTextEntry={true}
-                    onChange={(text) => setPassword(text)}
-                    leftIconName="lock"
-                    leftIconColor="#000"
-                    leftIconSize={20}
-                    className="mb-32"
-                  />
-                    </ScrollView>
-                    </KeyboardAvoidingView>
-                </>
-              )}
-            </>
-      
-
-          <View>
-            <View className="h-8" />
-            <CustomButton
-              backgroundColor={primarycolor}
-              Textname={
-                currentStep === 2
-                  ? "Continue"
-                  : currentStep === 1
-                  ? "Verify"
-                  : "Send"
-              }
-              TextColor={whitecolor}
-              onPress={handleContinue}
+      {currentStep === 0 && (
+        <>
+          <View className="mb-5">
+            <CustomTextnumber
+              placeholder={"mobile number"}
+              placeholderTextColor={greycolortwo}
             />
           </View>
+        </>
+      )}
 
-          {/* Numeric Keyboard */}
-          {currentStep === 1 && (
-            <View className="absolute -bottom-5 z-50 items-center">
-              <Animated.View className="w-full h-[467px]" style={[animatedStyles]}>
-                <NumericKeyboard onPress={(value) => handleOtpInput(value)} />
-              </Animated.View>
-            </View>
-          )}
+      <>
+        {currentStep === 2 && (
+          <>
+            <KeyboardAvoidingView
+              behavior={Platform.OS === "ios" ? "padding" : "height"}
+              style={{ flex: 1 }}
+              keyboardVerticalOffset={Platform.OS === "ios" ? 10 : 0} // Increase offset here
+            >
+              <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                <View>
+                  <Text
+                    style={[Textstyles.text_medium]}
+                    className="mb-4 text-center"
+                  >
+                    Welcome to HealthApp
+                  </Text>
+                  <View className="w-full h-28 mb-5 flex justify-center items-center">
+                    <View className="w-[117.91px] justify-center flex items-center h-[109.36px] rounded-xl">
+                      <Image
+                        source={require("../../assets/images/pro.png")}
+                        resizeMode="contain"
+                        className="w-full"
+                      />
+                    </View>
+                  </View>
+                </View>
+
+                <CustomInputWithHeader
+                  headerText="First name"
+                  placeholder="Enter your first name"
+                  leftIconName="user"
+                  onChange={(text) => console.log(text)}
+                />
+                <View className="h-3" />
+                <CustomInputWithHeader
+                  headerText="Last name"
+                  placeholder="Enter your last name"
+                  leftIconName="user"
+                  onChange={(text) => console.log(text)}
+                />
+                <View className="h-3" />
+                <CustomInputWithHeader
+                  headerText="Email address"
+                  placeholder="Enter your email address"
+                  leftIconName="envelope"
+                  onChange={(text) => console.log(text)}
+                />
+                <View className="h-3" />
+                <CustomInputWithHeader
+                  headerText="Phone number"
+                  placeholder="Enter your phone number"
+                  leftIconName="phone"
+                  onChange={(text) => console.log(text)}
+                />
+                <View className="h-3" />
+                <CustomInputWithHeader
+                  headerText="Date of birth"
+                  placeholder="Enter your date of birth"
+                  leftIconName="calendar"
+                  onChange={(text) => console.log(text)}
+                />
+                <View className="h-3" />
+                <CustomInputWithHeader
+                  headerText="Gender"
+                  placeholder="Enter your Gender"
+                  leftIconName="user"
+                  onChange={(text) => console.log(text)}
+                />
+                <View className="h-3" />
+                <CustomInputpassword
+                  headerText="Password"
+                  placeholder="Enter your password"
+                  value={password}
+                  secureTextEntry={true}
+                  onChange={(text) => setPassword(text)}
+                  leftIconName="lock"
+                  leftIconColor="#000"
+                  leftIconSize={20}
+                />
+                <View className="h-3" />
+                <CustomInputpassword
+                  headerText="Confirm password"
+                  placeholder="Enter your password again"
+                  value={password}
+                  secureTextEntry={true}
+                  onChange={(text) => setPassword(text)}
+                  leftIconName="lock"
+                  leftIconColor="#000"
+                  leftIconSize={20}
+                  className="mb-32"
+                />
+              </ScrollView>
+            </KeyboardAvoidingView>
+          </>
+        )}
+      </>
+
+      <View>
+        <View className="h-8" />
+        <CustomButton
+          backgroundColor={primarycolor}
+          Textname={
+            currentStep === 2
+              ? "Continue"
+              : currentStep === 1
+                ? "Verify"
+                : "Send"
+          }
+          TextColor={whitecolor}
+          onPress={handleContinue}
+        />
+      </View>
+
+      {/* Numeric Keyboard */}
+      {currentStep === 1 && (
+        <View className="absolute -bottom-5 z-50 items-center">
+          <Animated.View className="w-full h-[467px]" style={[animatedStyles]}>
+            <NumericKeyboard onPress={(value) => handleOtpInput(value)} />
+          </Animated.View>
         </View>
-    
+      )}
+    </View>
   );
 }
