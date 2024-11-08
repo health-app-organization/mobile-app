96b0d4b7439d8b624045bef21bd0ebe407b6397d
Ayomide Akintan <emzyakints2005@gmail.com>
2024-11-08 11:43:32 +0100

    working on doctor dashboard

--- screens/mycomponents/doctor/extra-info/steps.js
+++ screens/mycomponents/doctor/extra-info/steps.js
@@ -1,12 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-export const Step1 = () => {
+export const Step1 = ({ handleNextStep }) => {
     return (
         <View>
             <Text>Step1</Text>
         </View>
-    )
-}
+    );
+};
 
-const styles = StyleSheet.create({})
+export const Step2 = ({ handleNextStep }) => {
+    return (
+        <View>
+            <Text>Step1</Text>
+        </View>
+    );
+};
+
+export const Step3 = ({ handleNextStep }) => {
+    return (
+        <View>
+            <Text>Step1</Text>
+        </View>
+    );
+};
+
+export const Step4 = ({ handleNextStep }) => {
+    return (
+        <View>
+            <Text>Step1</Text>
+        </View>
+    );
+};
+
+export const Step5 = ({ handleNextStep }) => {
+    return (
+        <View>
+            <Text>Step1</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
